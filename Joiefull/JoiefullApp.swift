c1a20dbde775fe986d79396ea7729ad4a57bf9c6
Elena <elenabougaran@gmail.com>
2025-07-29 15:46:38 +0200

    Initial Commit

---
+++ Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Ordinateur elena on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
